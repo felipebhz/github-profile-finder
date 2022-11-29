029293d95174c9f58e59c96b75ff16a80d9fc5df
Felipe Ferreira <fbhz85@gmail.com>
2022-11-29 16:40:13 -0300

    migrating to useReducer

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,5 +1,12 @@
 const GithubReducer = (state, action) => {
     switch (action.type) {
+        case 'GET_USERS':
+            return {
+                ...state,
+                users: action.payload,
+                loading: false
+            }
+            break
         default:
             return state
     }
